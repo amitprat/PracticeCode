963b2a01b88593a8adc6f3e38af747489f52b4d8
Amit Pratap Singh <amitprat@buffalo.edu>
2015-05-09 16:15:36 -0400

    Update printNumbersInSeqUsingTwoThreads1_UsingCondVariable.cpp
    Added method to print using only mutex locks.
    
    The method of using semaphore is same of mutex locks.

--- printNumbersInSeqUsingTwoThreads1_UsingCondVariable.cpp
+++ printNumbersInSeqUsingTwoThreads1_UsingCondVariable.cpp
@@ -106,3 +106,55 @@ int main()
 	return 0;
 }
 */
+
+/*
+Option  - 3
+
+Using Two mutex lock instead of condtional variable.
+It will take the sequential locks to avoid any deadlock
+
+
+#include	<iostream>
+#include    <stdio.h>
+#include	<pthread.h>
+#include	<algorithm>
+
+using namespace std;
+
+pthread_mutex_t mutex1 = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t mutex2 = PTHREAD_MUTEX_INITIALIZER;
+bool turn = true;
+int limit = 100;
+void *print_even(void *)
+{
+    int count = 0;
+    while(count <= limit) {
+        pthread_mutex_lock(&mutex1);
+        cout<<count<<"\t";
+        count +=2;
+        pthread_mutex_unlock(&mutex2);
+    }
+}
+void *print_odd(void *)
+{
+    int count = 1;
+    while(count <= limit) {
+        pthread_mutex_lock(&mutex2);
+        cout<<count<<"\t";
+        count +=2;
+        pthread_mutex_unlock(&mutex1);
+    }
+}
+int main()
+{
+	pthread_t t1, t2;
+    pthread_mutex_lock(&mutex2); //odd print thread is already blocked here
+	pthread_create(&t1,NULL,print_odd,NULL);
+	pthread_create(&t2,NULL,print_even,NULL);
+	pthread_join(t1,NULL);
+	pthread_join(t2,NULL);
+	pthread_mutex_unlock(&mutex1);
+    cout<<endl;
+	return 0;
+}
+*/
