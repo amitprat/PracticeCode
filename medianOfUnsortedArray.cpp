a892e027bf7a3385ddafa7fe499995ad28227a3f
Amit Pratap Singh <amitprat@buffalo.edu>
2015-05-12 18:03:08 -0400

    Create medianOfUnsortedArray.cpp

---
+++ medianOfUnsortedArray.cpp
@@ -0,0 +1,2 @@
+1) Sort the array and return ( n%2 && a[n/2] || (a[n/2-1] + a[n/2])/2 ); T = O(nlgn)
+2) Run selection algorithm to find (n-1)/2 smallest element which is same as lower median T = O(n)
