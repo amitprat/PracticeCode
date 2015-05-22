3a935420a6c3daf8bb7e4f5a65adec085bc8dcdc
Amit Pratap Singh <amitprat@buffalo.edu>
2015-05-22 00:48:42 -0400

    Create checkPalinPairInStringArr.cpp

---
+++ checkPalinPairInStringArr.cpp
@@ -0,0 +1,12 @@
+/*
+You are given a list of word. Find if two words can be joined to-gather to form a palindrome. eg Consider a list {bat, tab, cat} Then bat and tab can be joined to gather to form a palindrome. 
+
+Expecting a O(nk) solution where n = number of works and k is length 
+
+There can be multiple pairs
+*/
+
+bool check(string words[],int n)
+{
+
+}
