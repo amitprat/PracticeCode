3efabbb828c12c12dd379d997ed5fdf4e3db8964
Amit Pratap Singh <amitprat@buffalo.edu>
2015-05-26 18:06:20 -0400

    Create horseMoveProblemOnChessBoard.cpp

---
+++ horseMoveProblemOnChessBoard.cpp
@@ -0,0 +1,4 @@
+/*
+You have a chess board of size NxN. You have a horse at a given starting position. You also have a function that gives you all the positions that the horse can reach from it's current position. 
+Given an ending position, find the path to it that uses the minimum number of moves.
+*/
