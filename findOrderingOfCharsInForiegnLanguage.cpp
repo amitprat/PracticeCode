c6eca9961c7b31473f4d489ccf4cf10528eb6895
Amit Pratap Singh <amitprat@buffalo.edu>
2015-05-28 13:57:18 -0400

    Create findOrderingOfCharsInForiegnLanguage.cpp

---
+++ findOrderingOfCharsInForiegnLanguage.cpp
@@ -0,0 +1,5 @@
+Q2 : Find the ordering of alphabets in foriegn language. Given some sorted words.
+  Sol :
+    compare each pair of words and break where their character is different.
+    insert an edge between char set [a-b]
+    do topo
