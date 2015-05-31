98a82ce62b5c5f7b3c85c7079dc69740ede6b469
Amit Pratap Singh <amitprat@buffalo.edu>
2015-05-30 22:28:52 -0400

    Update nextBiggerSparseNumber.cpp

--- nextBiggerSparseNumber.cpp
+++ nextBiggerSparseNumber.cpp
@@ -24,4 +24,57 @@ i.e"10101", "100000")
 - Check for highest index of a sequence of at least 2 ones (i.e "10011010001" - index 6) then we will see if we can put a 1 at starting index after sequence and set remaining bits to zero on the right side. (i.e: "10011010001" to "10100000000") 
 If we find out we created a new seq. of 2 one's on that index (i.e: "10110010001" to "11000000000") then we keep flipping bits to 0 until we get to a point where previous bit is 0 and next is 1 (i.e "101xx") which is sparse, or 0. 
 
+
+#include    <iostream>
+using namespace std;
+
+string convert(string str,int n)
+{
+    int i;
+    for(i=n-1;i>0;i--) {
+        if(str[i] == '0' && str[i-1] == '0')
+            break;
+    }
+    if(i == 0)  return ('1'+string(n,'0'));
+
+    str[i] = '1';
+    if(i == n-1)    {
+        return str;
+    }
+    str[i++] = '1';
+    while(i<n) str[i++] = '0';
+    return str;
+}
+bool isSparse(string str,int n) {
+    for(int i=0;i<n-1;i++)
+        if(str[i] == '1' && str[i+1] == '1')
+            return false;
+    return true;
+}
+string nonSparseConvert(string str,int n){
+    int i;
+    for(i=n-1;i>0;i--) {
+        if(str[i] == '1' && str[i-1] == '1')
+            break;
+    }
+    if(i == 0)  return str;
+    i -= 2;
+    for(;i>0;i--) {
+        if(str[i]=='0' && str[i-1] == '0')
+            break;
+    }
+    if(i == 0)  return ('1'+string(n,'0'));
+    return nonSparseConvert( (string(str,0,i) + '1' + string(n-i-1,'0')), n);
+}
+int main()
+{
+    string str("100110");
+    if(isSparse(str,str.length()))
+        cout<< convert(str,str.length())<<endl;
+    else
+        cout<< nonSparseConvert(str,str.length())<<endl;
+    return 0;
+}
+
+
 - If we get 0 then we just need to left shift 1 by the original number's length, example: "110010010" to "1000000000" (512). - Next power of 2. 
