68ceea5c2a074decba637489893b7652d18ad954
Amit Pratap Singh <amitprat@buffalo.edu>
2015-05-31 21:13:47 -0400

    Create findNumbersInInfiniteSortedArr.cpp

---
+++ findNumbersInInfiniteSortedArr.cpp
@@ -0,0 +1,6 @@
+/*
+Suppose you have an array with infinite numbers, which is sorted and there may be duplicates. Find the occurrence of a number.
+*/
+
+Double the index until you find a larger element, then binary search between the last two indices.
+like the skip list
