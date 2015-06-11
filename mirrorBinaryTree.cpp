eaa2901e0c3ab3100e288c5f1a8321f46ec40c1d
Amit Pratap Singh <amitprat@buffalo.edu>
2015-06-11 12:41:29 -0400

    Update mirrorBinaryTree.cpp

--- mirrorBinaryTree.cpp
+++ mirrorBinaryTree.cpp
@@ -3,13 +3,25 @@
     2   1       -->     1       2     -->     1   2
   4       5               5   4             5       4
 */
+/*construct seperate mirror tree*/
 tree *mirror(tree *orig)
 {
   if(!orig) return NULL;
-  
   tree *newTree = new tree(orig->data);
   newTree->left = mirror(orig->right);
   newTree->right = mirror(orig->left);
   
   return newTree;
 }
+
+/*inplace swapping*/
+void swap_node(node n) {
+  if(n != null) {
+    swap(n.left,n.right);
+
+    swap_node(n.left);
+    swap_node(n.right);
+  }
+}
+
+swap_node(root);
