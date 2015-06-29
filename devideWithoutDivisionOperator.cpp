b3878d9aa668fdaf6640849dad29081a9556fc80
Amit Pratap Singh <amitprat@buffalo.edu>
2015-06-29 00:37:42 -0400

    Create devideWithoutDivisionOperator.cpp

---
+++ devideWithoutDivisionOperator.cpp
@@ -0,0 +1,25 @@
+//taken from stackoverflow
+int divide(int nu, int de) {
+
+    int temp = 1;
+    int quotient = 0;
+
+    while (de <= nu) {
+        de <<= 1;
+        temp <<= 1;
+    }
+
+    //printf("%d %d\n",de,temp,nu);
+    while (temp > 1) {
+        de >>= 1;
+        temp >>= 1;
+
+        if (nu >= de) {
+            nu -= de;
+            //printf("%d %d\n",quotient,temp);
+            quotient += temp;
+        }
+    }
+
+    return quotient;
+}
