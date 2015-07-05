cb95068a64bcf92e1451d0d98be352d7199cef48
Amit Pratap Singh <amitprat@buffalo.edu>
2015-07-05 17:54:27 -0400

    Update wildCarMatching.cpp

--- wildCarMatching.cpp
+++ wildCarMatching.cpp
@@ -10,35 +10,58 @@ bool match(char *first, char * second)
     if (*first == '\0' && *second == '\0')
         return true;
     /*
-    Note - this is not correct condtion to handle case first = "*", second = "" or any first string ending with '*'
-    if we want to put below condition then it should be , we should put this check first
-    if(*first == '*' && *(first+1) == '\0' && *second = '\0')
-      return true;
+    this check fails for the case , s = ""; p = "*"; 
+    if(!*first || !*second) return false;
+    
+    The below case check specially for s="", p = "*x";
+    
+    If we want to go in first way of return false,
+    put extra check to return true;
+    if(*first == '*' && *(first+1)=='\0' && !*second)
+     return true;
     */
-    //if( *first == '\0' || *second == '\0') 
-    //  return false;
-    // Make sure that the characters after '*' are present in second string.
-    // This function assumes that the first string will not contain two
-    // consecutive '*' 
     if (*first == '*' && *(first+1) != '\0' && *second == '\0')
         return false;
  
-    // If the first string contains '?', or current characters of both 
-    // strings match
     if (*first == '?' || *first == *second)
         return match(first+1, second+1);
  
-    // If there is *, then there are two possibilities
-    // a) We consider current character of second string
-    // b) We ignore current character of second string.
     if (*first == '*')
         return match(first+1, second) || match(first, second+1);
+        
     return false;
 }
- 
+
+/*Dynamic Programming*/
+bool isMatch(char *p, char *s)
+{
+    int plen = strlen(p);
+    int slen = strlen(s);
+    int table[plen+1][slen+1];
+    for(int i=0;i<=plen;i++)
+    {
+        for(int j=0;j<=slen;j++) {
+            table[i][j] = 0;
+            if(i == 0 && j == 0 || (i && p[i-1] == '*') )
+                table[i][j] = 1;
+            if(i && j){
+                if( p[i-1] == s[j-1] || (p[i-1] == '.') )
+                    table[i][j] = table[i-1][j-1];
+            }
+        }
+    }
+    /*for(int i=0;i<=plen;i++) {
+        for(int j=0;j<=slen;j++)
+            printf("%d ",table[i][j]);
+        printf("\n");
+    }*/
+    return table[plen][slen];
+}
 // A function to run test cases
 void test(char *first, char *second)
-{  match(first, second)? puts("Yes"): puts("No"); }
+{  
+  match(first, second)? puts("Yes"): puts("No"); 
+}
  
 // Driver program to test above functions
 int main()
