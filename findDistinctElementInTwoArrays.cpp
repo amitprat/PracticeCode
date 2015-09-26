647e2f8e393711baae61dee9a409bc33f965e2fb
Amit Pratap Singh <amitprat@buffalo.edu>
2015-09-26 01:04:54 -0400

    Create findDistinctElementInTwoArrays.cpp

---
+++ findDistinctElementInTwoArrays.cpp
@@ -0,0 +1,7 @@
+given two arrays of integers of sizes m and n respectively, find the distinct elements in the two arrays in less than O(mlogm + nlogn + m+n)
+
+
+Try using hash tables.
+Try sorting one array and binary searching over it.
+
+Runtime you posted in question suggests we have to beat the idea of sorting both arrays.
