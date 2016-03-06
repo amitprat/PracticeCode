05085a95e1aa1abdb386a91a703cd6a74643f28c
Amit Pratap Singh <amitprat@buffalo.edu>
2016-03-06 10:51:17 -0800

    Update Minheap.cpp

--- Minheap.cpp
+++ Minheap.cpp
@@ -1,73 +1,113 @@
-#include <iostream>
-#include <fstream>
-#include <sstream>
-#include <algorithm>
-
-#define DEFAULT_SIZE    10
-using namespace std;
-
+#include "Minheap.h"
 template <class T>
-class Minheap
+int Minheap::left(int i)
 {
-public:
-    T *arr;
-    int size;
-    int capacity;
-    int left(int i) {return 2*i +1; }
-    int right(int i) { return 2*i + 2 ;}
-    int parent(int i) { return i/2; }
-    void heapify(int i=0)
+    return 2*i+1;
+}
+template <class T>
+int Minheap::right(int i)
+{
+    return 2*i+2;
+}
+template <class T>
+int Minheap::parent(int i)
+{
+    return i/2;
+}
+template <class T>
+void Minheap::heapify(int i)
+{
+    int smallest = i;
+    int l = left(i);
+    int r = right(i);
+    if(heap[l] < heap[i] && l<cur_size)
+        smallest = l;
+    if(heap[r] < heap[smallest] && r<cur_size)
+        smallest = r;
+    if(smallest != i)
     {
-        int l = left(i);
-        int r = right(i);
-        int smallest = i;
-        if(l < size && arr[l] < arr[smallest])
-            smallest = l;
-        if(r < size && arr[r] < arr[smallest])
-            smallest = r;
-        if(smallest != i) {
-            swap(arr[smallest],arr[i]);
-            heapify(smallest);
-        }
+        swap(&heap[i],&heap[smallest]);
+        heapify(heap,smallest,cur_size);
     }
-    Minheap(int size=DEFAULT_SIZE)
+}
+template <class T>
+void Minheap::build_max_heap()
+{
+    int i=0;
+    for(i=cur_size/2; i>=0; i--)
+        heapify(heap,i,cur_size);
+}
+template <class T>
+bool Minheap::Minheap::isFull()
+{
+    return (cur_size == heap_size);
+}
+template <class T>
+void Minheap::resizeHeap()
+{
+    T *newheap = new T[heap_size*2];
+    for(int i=0; i<heap_size; i++)
+        new_heap[i] = heap[i];
+    heap_size *= 2;
+    delete[] heap;
+    heap = newheap;
+}
+template <class T>
+Minheap::Minheap(int sz = HEAP_SIZE) : heap_size(sz)
+{
+    a = new T[sz];
+    cur_size = 0;
+}
+template <class T>
+Minheap::~Minheap()
+{
+    delete T;
+}
+template <class T>
+bool Minheap::isEmpty()
+{
+    return (cur_size == 0);
+}
+template <class T>
+void Minheap::push(T key)
+{
+    if( isFull() )
+        resizeHeap();
+    cur_size++;
+    heap[cur_size] = MAX;
+    decreaseKey(key);
+    cur_size++;
+}
+template <class T>
+T Minheap::top()
+{
+    return a[0];
+}
+template <class T>
+void Minheap::pop()
+{
+    heap[0] = heap[cur_size-1];
+    cur_size--;
+    decrease_key(--cur_size);
+}
+template <class T>
+T Minheap::extractMin()
+{
+    if(cur_size < 0)
+        return -1;
+    T max_ = top();
+    pop();
+    return max_;
+}
+template <class T>
+void Minheap::decreaseKey(int k)
+{
+    if(k > heap[i])
+        return;
+    heap[i] = k;
+    while(i>0 && heap[parent(i)] < heap[i])
     {
-        arr = new T[capacity=size];
-        this->size = 0;
+        swap(&heap[i],&heap[parent(i)]);
+        i = parent(i);
     }
-    Minheap(T arr[], int size) {
-        size = 0;
-        arr = new T[capacity=size];
-        for(int i=0;i<size;i++)
-            this->arr[i] = arr[i];
-        heapify();
-    }
-    void push(T item) {
-        if(size >= capacity) return;
-        arr[size++] = item;
-        heapify();
-    }
-    T extractMin() {
-        T top = arr[0];
-        arr[0] = arr[size-1];   size--;
-        heapify();
-        return top;
-    }
-    void decreaseKey(int pos, T key)
-    {
-        if( arr[pos] < key )
-            return;
-        arr[pos] = key;
-        while(pos > 0 && arr[parent(pos)] > arr[pos]) {
-            swap(arr[parent(pos)],arr[pos]);
-            pos=parent(pos);
-        }
-    }
-    void insert(T item) {
-        arr[size++] = item;
-        decreaseKey(size-1,item);
-    }
-    T getMin()  { return arr[0]; }
-    bool empty() { return (size<=0); }
-    bool full() { return (size == capacity);}
-};
+}
