5bef0a4d941898546eb915371c4d33a386537ea1
Amit Pratap Singh <amitprat@buffalo.edu>
2020-12-18 11:02:01 -0800

    Update sumFindProbem_pair_triplet_quadruplets.cpp

--- sumFindProbem_pair_triplet_quadruplets.cpp
+++ sumFindProbem_pair_triplet_quadruplets.cpp
@@ -14,6 +14,14 @@ struct Node
 };
 
 /* taken from careercup */
+/*
+    You're given an array of integers(eg [3,4,7,1,2,9,8]) Find the index of values that satisfy A+B = C + D, where A,B,C & D are integers values in the array.
+
+    Eg: Given [3,4,7,1,2,9,8] array
+    The following
+    3+7 = 1+ 9 satisfies A+B=C+D
+    so print (0,2,3,5)
+*/
 vector< vector<int> > Print4Sum(vector<int> A){
     int tsum, l = A.size();
     vector< vector<int> > ans;
